4f6a480f2ec22de36f77ece7178df351749babcd
lyn han <lynfhan@gmail.com>
2016-04-25 22:28:32 -0500

    move notif to left of login

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -27,143 +27,141 @@ import toastr from 'toastr';
 @withStyles(s)
 class Navigation extends Component {
 
-  static propTypes = {
-    className: PropTypes.string,
-  };
+    static propTypes = {
+        className: PropTypes.string,
+    };
 
-  constructor(props) {
-    super(props);
-    this.state = objectAssign(ProfileStore.getState(),
-    { showModal: false, showRegisterModal: false});
-    // need to use bind so that the this variable for onChange
-    // refers to this DeckPage object not the function
-    this.onChange = this.onChange.bind(this);
-    this.openLogInModal = this.openLogInModal.bind(this);
-    this.closeLogInModal = this.closeLogInModal.bind(this);
-    this.openRegisterModal = this.openRegisterModal.bind(this);
-    this.closeRegisterModal = this.closeRegisterModal.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = objectAssign(ProfileStore.getState(),
+        { showModal: false, showRegisterModal: false});
+        // need to use bind so that the this variable for onChange
+        // refers to this DeckPage object not the function
+        this.onChange = this.onChange.bind(this);
+        this.openLogInModal = this.openLogInModal.bind(this);
+        this.closeLogInModal = this.closeLogInModal.bind(this);
+        this.openRegisterModal = this.openRegisterModal.bind(this);
+        this.closeRegisterModal = this.closeRegisterModal.bind(this);
+    }
 
-  componentDidMount() {
-    // makes the DeckStore call the onchange function whenever it cnanges.
-    // This is why we had to use bind
-    ProfileStore.listen(this.onChange);
-    // $.notify({
-    //   title: "Welcome:",
-    //   message: "src\components\Navigation\Navigation.js to change this"
-    // });
-    // As soon as it is poling for data get data
-  }
+    componentDidMount() {
+        // makes the DeckStore call the onchange function whenever it cnanges.
+        // This is why we had to use bind
+        ProfileStore.listen(this.onChange);
+        // $.notify({
+        //   title: "Welcome:",
+        //   message: "src\components\Navigation\Navigation.js to change this"
+        // });
+        // As soon as it is poling for data get data
+    }
 
-  componentWillUnmount() {
-    // remove event listener
-    ProfileStore.unlisten(this.onChange);
-  }
+    componentWillUnmount() {
+        // remove event listener
+        ProfileStore.unlisten(this.onChange);
+    }
 
-  openLogInModal() {
-    this.setState({ showModal: true });
-  }
-  closeLogInModal() {
-    this.setState({ showModal: false });
-  }
+    openLogInModal() {
+        this.setState({ showModal: true });
+    }
+    closeLogInModal() {
+        this.setState({ showModal: false });
+    }
 
 
-  logIn(event) {
-    event.preventDefault();
-    const username = event.target[0].value;
-    const password = event.target[1].value;
-    ProfileActions.logIn({ username, password });
-    this.closeLogInModal();
-  }
+    logIn(event) {
+        event.preventDefault();
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+        ProfileActions.logIn({ username, password });
+        this.closeLogInModal();
+    }
 
-  logOut() {
-    ProfileActions.logOut();
-  }
+    logOut() {
+        ProfileActions.logOut();
+    }
 
-  openRegisterModal() {
-    this.setState({showRegisterModal: true});
-  }
-  closeRegisterModal() {
-    this.setState({showRegisterModal: false});
-  }
+    openRegisterModal() {
+        this.setState({showRegisterModal: true});
+    }
+    closeRegisterModal() {
+        this.setState({showRegisterModal: false});
+    }
 
-  register(event) {
-    event.preventDefault();
-    const username = event.target[0].value;
-    const password = event.target[1].value;
-    ProfileActions.signUp({ username, password });
-    this.closeRegisterModal();
-  }
+    register(event) {
+        event.preventDefault();
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+        ProfileActions.signUp({ username, password });
+        this.closeRegisterModal();
+    }
 
-  onChange(state) {
-    this.setState(state)
-  }
+    onChange(state) {
+        this.setState(state)
+    }
 
-  render() {
-    const LogInModalButton = (!this.state.loggedIn)?
-    <NavItem onClick = {this.openLogInModal}>Log In</NavItem> : <NavItem onClick = {this.logOut}>Log Out</NavItem>
-    const RegisterModalButton = (!this.state.loggedIn) ? <NavItem onClick = {this.openRegisterModal}>Register</NavItem> : null;
-    const numNotifications = this.state.transactions.filter(t => !t.acknowledged).length;
-    const NotificationButton = (numNotifications) ? <NavItem>
-    Notifications <Badge>{numNotifications}</Badge>
-</NavItem> : <NavItem />;
+    render() {
+        const LogInModalButton = (!this.state.loggedIn)?
+        <NavItem onClick = {this.openLogInModal}>Log In</NavItem> : <NavItem onClick = {this.logOut}>Log Out</NavItem>
+        const RegisterModalButton = (!this.state.loggedIn) ? <NavItem onClick = {this.openRegisterModal}>Register</NavItem> : null;
+        const numNotifications = this.state.transactions.filter(t => !t.acknowledged).length;
+        const NotificationButton = (numNotifications) ? <NavItem> Notifications <Badge>{numNotifications}</Badge></NavItem> : null;
 
 return (
-  <Navbar>
+    <Navbar>
 
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a className={s.link} href="/" onClick={Link.handleClick}>AnkiHub</a>
-      </Navbar.Brand>
-      <Navbar.Toggle/>
-    </Navbar.Header>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a className={s.link} href="/" onClick={Link.handleClick}>AnkiHub</a>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+        </Navbar.Header>
 
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem>{this.props.children}</NavItem>
-        <NavItem>{this.state.loggedIn}</NavItem>
-      </Nav>
-      <Nav pullRight>
-        <NavItem href="/profile" onClick={Link.handleClick}>Profile</NavItem>
-        <NavItem href="/decks" onClick={Link.handleClick}>Decks</NavItem>
-        {LogInModalButton}
-        {RegisterModalButton}
-        {NotificationButton}
-      </Nav>
-    </Navbar.Collapse>
+        <Navbar.Collapse>
+            <Nav>
+                <NavItem>{this.props.children}</NavItem>
+                <NavItem>{this.state.loggedIn}</NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavItem href="/profile" onClick={Link.handleClick}>Profile</NavItem>
+                <NavItem href="/decks" onClick={Link.handleClick}>Decks</NavItem>
+                {NotificationButton}
+                {LogInModalButton}
+                {RegisterModalButton}
+            </Nav>
+        </Navbar.Collapse>
 
-    <Modal show={this.state.showModal} onHide = { this.closeLogInModal } className={s.left}>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-lg">LogIn to AnkiHub</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={ this.logIn.bind(this) }>
-          <Input type="text" label="Username" placeholder="Username" ref="usernameField" value={this.state.username} />
-          <Input type="password" label="Password" ref="passwordField" />
-          <ButtonInput type="submit" value="Log In" />
-        </form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={this.closeLogInModal}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+        <Modal show={this.state.showModal} onHide = { this.closeLogInModal } className={s.left}>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-lg">LogIn to AnkiHub</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form onSubmit={ this.logIn.bind(this) }>
+                    <Input type="text" label="Username" placeholder="Username" ref="usernameField" value={this.state.username} />
+                    <Input type="password" label="Password" ref="passwordField" />
+                    <ButtonInput type="submit" value="Log In" />
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.closeLogInModal}>Close</Button>
+            </Modal.Footer>
+        </Modal>
 
-    <Modal show={this.state.showRegisterModal} onHide = { this.closeRegisterModal } className={s.left}>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-lg">Register for AnkiHub</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={ this.register.bind(this) }>
-          <Input type="text" label="Username" placeholder="Username" ref="usernameField" value={this.state.username} />
-          <Input type="password" label="Password" ref="passwordField" />
-          <ButtonInput type="submit" value="Register" />
-        </form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={this.closeRegisterModal}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  </Navbar>
+        <Modal show={this.state.showRegisterModal} onHide = { this.closeRegisterModal } className={s.left}>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-lg">Register for AnkiHub</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form onSubmit={ this.register.bind(this) }>
+                    <Input type="text" label="Username" placeholder="Username" ref="usernameField" value={this.state.username} />
+                    <Input type="password" label="Password" ref="passwordField" />
+                    <ButtonInput type="submit" value="Register" />
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={this.closeRegisterModal}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    </Navbar>
 );
 }
 
